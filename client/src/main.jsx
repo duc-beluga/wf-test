c755fbb7e8fe0461e26bd6fbf41cda34bceaf470
duc-beluga <dnguyen416@gatech.edu>
2024-03-13 08:45:25 -0400

    ade

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { useRoutes } from "react-router-dom";
+import Contact from "../pages/Contact.jsx";
+import Home from "../pages/Home.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+let routes = useRoutes([
+  { path: "/wf-test/", element: <Home /> },
+  { path: "/wf-test/contact", element: <Contact /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>{routes}</BrowserRouter>
+  </React.StrictMode>
+);
